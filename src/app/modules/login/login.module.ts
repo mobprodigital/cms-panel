7603923a2186a1258e0abfdd5e99a0912caf7ea4
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-10-08 18:09:50 +0530

    login page UI added

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing/login-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+//material modules
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
   ],
   declarations: [LoginComponent],
   exports: [LoginRoutingModule]
