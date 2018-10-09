9988f16b5aa9aa428820435064143514587ac04b
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-10-09 12:35:23 +0530

    sidebar, nav and create user addded

--- src/app/modules/panel/modules/user-account/user-account.module.ts
+++ src/app/modules/panel/modules/user-account/user-account.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { UserAccountRoutingModule } from './user-account-routing/user-account-routing.module';
 import { UserAccountComponent } from './components/user-account/user-account.component';
+import { MatCardModule, MatInputModule, MatSelectModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserAccountRoutingModule
+    UserAccountRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   declarations: [UserAccountComponent, CreateUserComponent],
-  exports: [UserAccountRoutingModule]
+  exports: [UserAccountRoutingModule, MatCardModule]
 })
 export class UserAccountModule { }
--- src/app/modules/panel/panel.module.ts
+++ src/app/modules/panel/panel.module.ts
@@ -3,12 +3,27 @@ import { CommonModule } from '@angular/common';
 import { PanelComponent } from './components/panel/panel.component';
 import { PanelRoutingModule } from './panel-routing/panel-routing.module';
 
+import {
+  MatExpansionModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule
+} from '@angular/material';
+
+
 @NgModule({
   imports: [
     CommonModule,
-    PanelRoutingModule
+    PanelRoutingModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   declarations: [PanelComponent],
-  exports : [PanelRoutingModule]
 })
 export class PanelModule { }
